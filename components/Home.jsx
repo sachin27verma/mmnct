84660986d469dec8f1656f1a761ef8bd2e75f98c
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2023-10-15 20:55:04 +0530

    Homecard changed

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import HomeCard from "./HomeCard";
-import { VscCircleFilled } from "react-icons/vsc";
 
 // import { SiGooglemaps } from "react-icons/si";
 // import Banner from "./Banner";
@@ -26,25 +25,6 @@ function HomePage({ teamlist }) {
     setScreenSize(window.innerWidth);
     setWinnerTeamList(() => decideWinner("SHM", "SAM", teamlist));
   }, []);
-
-  const over_summary = [1, 4, 6, "W", "2NB", 0, "2WD", 3, "W", 2, 1];
-
-  const over = over_summary.map((bowl_value) => (
-    <div
-      key={bowl_value}
-      className={`h-[25px] w-[25px] text-xs font-semibold ${
-        bowl_value === "W"
-          ? "bg-red-600"
-          : bowl_value === 4 || bowl_value === 6
-          ? "bg-[#6360FF]"
-          : bowl_value.toString().slice(-2).toUpperCase() === "WD" ||
-            bowl_value.toString().slice(-2).toUpperCase() === "NB"
-          ? "bg-green-500 text-[10px]"
-          : "bg-gray-500"
-      }  flex justify-center items-center rounded-full text-white ring-2 ring-[#F9BD48]`}>
-      <span className="  ">{bowl_value}</span>
-    </div>
-  ));
 
   const [live, setlive] = useState(true);
 
@@ -75,101 +55,11 @@ function HomePage({ teamlist }) {
         {/* graphic and venue along with days to go */}
 
         <div>
-          {/* <HomeCard /> */}
+          {/* */}
           {live ? (
-            <div className=" w-4/5 md:w-[400px] days-counter mx-auto md:mx-0 md:mb-4 md:gap-4 h-auto ">
-              <div className=" w-full my-2 px-2">
-                <div className=" flex justify-between text-black items-center">
-                  <p className="  text-2xl font-extrabold text-[#F8C156]">
-                    {" "}
-                    <span className=" text-red-600 rounded-full">
-                      <VscCircleFilled className=" inline-block animate-ping text-xl bg-red-300 overflow-hidden rounded-full" />
-                    </span>
-                    Live
-                  </p>
-                  <p className=" text-sm font-bold text-[#F8C156]">
-                    {/* Match Date */}
-                    29<span className=" font-thin">th</span> oct, 2023
-                  </p>
-                </div>
-              </div>
-              <div className=" teams flex px-2 mb-1">
-                <div className=" team_1 flex justify-around  w-[45%]">
-                  <div className=" flex justify-center text-center  flex-col  ">
-                    <div>
-                      {/* team 1 image */}
-                      <Image
-                        src={`/male.jpg`}
-                        height={50}
-                        width={50}
-                        alt="team_1_logo"
-                        className=" rounded-full aspect-square"
-                      />
-                    </div>
-                    {/* Team 1 name info */}
-                    <p className=" text-black text-sm font-bold">AVG</p>
-                    <p className=" text-black text-xs font-thin">Btech-III</p>
-                  </div>
-                  <div className=" score text-red-600 translate-y-4">
-                    {/* team 1 score */}
-                    <p className=" text-sm font-bold">
-                      120-6<span className=" text-xs font-thin">(7.5)</span>{" "}
-                    </p>
-                  </div>
-                </div>
-                <div className=" w-[10%]  ">
-                  <hr className=" h-1 bg-[#F8C156] rotate-90 translate-y-6 w-full" />
-                </div>
-
-                <div className=" team_2 flex flex-row-reverse justify-around  w-[45%]">
-                  <div className=" flex justify-center text-center  flex-col  ">
-                    <div>
-                      {/* team 2 logo image */}
-                      <Image
-                        src={`/female.jpg`}
-                        height={50}
-                        width={50}
-                        alt="team_2_logo"
-                        className=" rounded-full aspect-square"
-                      />
-                    </div>
-                    {/* team 2 name info */}
-                    <p className=" text-black text-sm font-bold">SMK</p>
-                    <p className=" text-black text-xs font-thin">B-IV</p>
-                  </div>
-                  <div className=" score  text-red-600 translate-y-4">
-                    <p className=" text-sm font-bold">
-                      {/* team 2 status */}
-                      <span className=" text-xs font-thin">Yet to bat</span>{" "}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <hr className=" h-0 border-dashed bg-[#F8C156]   w-[95%] mx-auto" />
-              <div className=" Bat_and_bowl_stats flex justify-between text-black px-3 mb-1">
-                <div className=" Bat_stat text-left  ">
-                  {/* batting team stats -- striker and non-striker */}
-                  <p className=" text-sm font-bold mb-1">IMR Batting</p>
-                  <p className=" text-xs font-light">Player x: 34*(49)</p>
-                  <p className="text-xs font-light">Player x: 12*(21)</p>
-                </div>
-                <div className=" Bowl_stat text-right">
-                  {/* Active Bowler info */}
-                  <p className=" text-sm font-bold">AVG-Bowling</p>
-                  <p className=" text-xs font-light">Player x: 1/18 (3.3)</p>
-                </div>
-              </div>
-              <hr className=" h-0 border-dashed bg-[#F8C156]  w-[95%] mx-auto" />
-              <div className="Over pb-3 px-2">
-                <div className=" flex justify-center text-sm mb-1 font-bold  text-black ">
-                  THIS OVER
-                </div>
-                <div className=" Over_detail flex justify-center flex-wrap gap-4 ">
-                  {/* input of per ball */}
-                  {over}
-                </div>
-              </div>
-            </div>
+            <>
+              <HomeCard />{" "}
+            </>
           ) : (
             <div className=" flex justify-center items-center w-4/5 md:w-[400px] days-counter mx-auto md:mx-0 md:mb-4 md:gap-4 h-[116px] md:h-[150px]">
               <span className="text-[#F45178] font-[800] text-[96px] md:-[120px] leading-[117px] md:leading-[146.28px] text-center w-1/2 md:w-[35%]">
