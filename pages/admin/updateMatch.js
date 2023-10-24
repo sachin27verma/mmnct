2b21f7fa678d60af1ae758b6da77a1d56670c60f
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2023-10-24 23:32:06 +0530

    fixed button color : updateMatch

--- pages/admin/updateMatch.js
+++ pages/admin/updateMatch.js
@@ -295,7 +295,7 @@ const handleEmergencyButton =async(e)=>{
             />
           </div>
         </div>
-        <form class="w-full max-w-sm mx-auto px-4" method="POST">
+        <form class="w-full max-w-[700px]  mx-auto px-4" method="POST">
           {/* current Batting team */}
           <div class="md:flex md:items-center mb-6">
             <div class="md:w-1/3">
@@ -442,7 +442,7 @@ const handleEmergencyButton =async(e)=>{
             <div class="md:w-1/3"></div>
             <div class="md:w-2/3">
               <button
-                class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                 type="submit"
                 // onClick={handleForcedStrikerChange}
                 onClick={
@@ -505,7 +505,21 @@ const handleEmergencyButton =async(e)=>{
               />
             </div>
           </div>
-
+              <div className=" flex justify-between w-full">
+         
+          <div class="md:flex md:items-center">
+            <div class="md:w-1/3"></div>
+            <div class="md:w-2/3">
+              <button
+                class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit"
+                onClick={changeMatchInnings}
+              >
+                Change Innings
+              </button>
+            </div>
+          </div>
+          <br></br>
           <div class="md:flex md:items-center">
             <div class="md:w-1/3"></div>
             <div class="md:w-2/3">
@@ -525,24 +539,12 @@ const handleEmergencyButton =async(e)=>{
               <button
                 class="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                 type="submit"
-                onClick={changeMatchInnings}
-              >
-                Change Innings
-              </button>
-            </div>
-          </div>
-          <br></br>
-          <div class="md:flex md:items-center">
-            <div class="md:w-1/3"></div>
-            <div class="md:w-2/3">
-              <button
-                class="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                type="submit"
                 onClick= {handleEmergencyButton}
               >
                 Emergency Button
               </button>
             </div>
+          </div>
           </div>
         </form >
       </div >
